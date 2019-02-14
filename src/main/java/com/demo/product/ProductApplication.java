2b15bf1ea2f7d0b0dc21c3ccf2a0c56c5719df8c
mooodo <fangang@aisino.com>
2019-02-14 21:20:12 +0800

    add enable hystrix in application.

--- src/main/java/com/demo/product/ProductApplication.java
+++ src/main/java/com/demo/product/ProductApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
@@ -19,6 +20,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
 @Configuration
 @ImportResource(locations={"classpath:applicationContext.xml"})
 public class ProductApplication extends SpringBootServletInitializer {
